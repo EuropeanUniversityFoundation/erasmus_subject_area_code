797d43d071a839a074a4eec815ab93314ce66b91
Tiago Simões <tiago.simoes@uni-foundation.eu>
2024-06-11 17:02:16 +0200

    Switches file include to static call

---
+++ src/ErasmusSubjectAreaCodesInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Drupal\erasmus_subject_area_code;
+
+interface ErasmusSubjectAreaCodesInterface {
+
+  /**
+   * Curated list of Erasmus Subject Area codes.
+   *
+   * @return array
+   */
+  public static function codes(): array;
+
+}
